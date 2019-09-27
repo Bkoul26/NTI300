6a49b5037c0e2a921cead8abfb58ee1b64c43390
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-09-26 20:14:52 -0700

    Delete apache-automation.sh

--- apache-automation.sh
+++
@@ -1,5 +0,0 @@
-yum -y install httpd mod_ssl
-systemctl start httpd
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf
-echo "<html><body><h1>Hi there NTI 300</h1></body?></html>" > /var/www/html/index.html
-systemctl restart httpd
