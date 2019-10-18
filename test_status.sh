834d44a7dbbecf1162336d2e9bf64c6571453f78
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-10-17 18:46:06 -0700

    Update test_status.sh

--- test_status.sh
+++ test_status.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+ 
+installed =yum($yum list installed "httpd")
+yum list installed "httpd"
+if [ z "$1"]; then
+echo "This package is installed"
+  exit 0;
+  fi
+  
+  
+  
+ echo "Look for package? [Y,n]"
+read input
+if {{$input == "Y" || $input =="y"]]; then
+  echo "Look for package"
+  installed =yum($yum list installed $input || grep$1)
+yum list installed $input
+if [ z "$1"]; then
+echo "This package is installed"
+else
+    echo "search terminated"
+    exit 0;
+ fi
+
+  
+  
+  
+  echo "Do that? [Y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "do that"
+else
+        echo "don't do that"
+fi
+
+
+
+
+
 if [-z " $1"]; then
 echo "you didn't provide an argument"
   exit 0;
