e53645f122b8ed72fea67fbb3e8830145922db39
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-10-24 19:57:03 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -6,11 +6,11 @@ pip install virtualenv
 pip install --upgrade pip 
 mkdir ~/newproject   
 cd ~/newproject   
-virtualenv 3 newenv   
+virtualenv newenv   
 source newenv/bin/activate   
 pip install django  
 django-admin --version   
-django-admin 3 startproject newproject  
+django-admin startproject newproject  
 cd newproject/
 python manage.py migrate
 python manage.py createsuperuser	
@@ -21,20 +21,23 @@ python manage.py runserver 0.0.0.0:8000
 
 
 #!/bin/bash
-
+yum clean all
 yum -y install python-pip
 pip install virtualenv
 pip install --upgrade pip 
 mkdir ~/newproject   
 cd ~/newproject   
-virtualenv 3 newenv   
+virtualenv  newenv   
 source newenv/bin/activate   
 pip install django  
 django-admin --version   
-django-admin 3 startproject newproject  
+django-admin startproject newproject  
 cd newproject/
 python manage.py migrate
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'password')" | python manage.py shell	
-newproject/setting.py	
+myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS=  \[\'$myip\'\]/g" newproject/settings.py
+###echo $myip
+###newproject/settings.py	
 python manage.py runserver 0.0.0.0:8000   
 
