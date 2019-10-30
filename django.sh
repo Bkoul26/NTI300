feca133be31dda271048bd788926ad5c52a86b03
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-10-30 14:58:42 -0700

    Update django.sh

--- django.sh
+++ django.sh
@@ -1,21 +1,21 @@
 
 #!/bin/bash
-
-yum -y install python-pip
-pip install virtualenv
-pip install --upgrade pip 
-mkdir ~/newproject   
-cd ~/newproject   
-virtualenv newenv   
-source newenv/bin/activate   
-pip install django  
-django-admin --version   
-django-admin startproject newproject  
-cd newproject/
-python manage.py migrate
-python manage.py createsuperuser	
-newproject/setting.py	
-python manage.py runserver 0.0.0.0:8000   
+#THIS IS A ROUGH DRAFT
+#yum -y install python-pip
+#pip install virtualenv
+#pip install --upgrade pip 
+#mkdir ~/newproject   
+#cd ~/newproject   
+#virtualenv newenv   
+#source newenv/bin/activate   
+#pip install django  
+#django-admin --version   
+#django-admin startproject newproject  
+#cd newproject/
+#python manage.py migrate
+#python manage.py createsuperuser	
+#newproject/setting.py	
+#python manage.py runserver 0.0.0.0:8000   
 
 
 
