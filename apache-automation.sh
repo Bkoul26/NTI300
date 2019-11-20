b598285b1f15bbfcf142b690f0cee22604b4013d
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-11-19 19:51:16 -0800

    Update apache-automation.sh

--- apache-automation.sh
+++ apache-automation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [ -e/etc/httpd ]; then   #my comment
- exit 0                         #another comment
- fi                              #closing statement
-yum -y install httpd mod_ssl                                                                       # install apache and ssl support
-systemctl start httpd                                                                              # start apache     
-sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                    # comment out the welcome page 
-echo "<html><body><h1>Hi there NTI 300</h1></body?></html>" > /var/www/html/index.html             # create custom welcome page 
-systemctl restart httpd                                                                            # restart apache so changes take effect
+if [ -e/etc/httpd ]; then  
+ exit 0                    
+ fi                             
+yum -y install httpd mod_ssl                                                                       # install apache and ssl
+systemctl start httpd                                                                              # start apach
+sed -i 's/^/#/g' /etc/httpd/conf.d/welcome.conf                                                    # WELCOME PAGE 
+echo "<html><body><h1>Hi there NTI 300</h1></body?></html>" > /var/www/html/index.html             # CUSTOM WELCOME PAGE CREATED 
+systemctl restart httpd                                                                            # restart apache forr changes to take effect
