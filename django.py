38e70b3053a75b91c62a121d2dbc099388bc20c5
Bkoul26 <48499517+Bkoul26@users.noreply.github.com>
2019-12-04 17:51:23 -0800

    Update django.py

--- django.py
+++ django.py
@@ -1,56 +1,71 @@
 #!/usr/bin/python
 
 import os
+import re
 import subprocess
 
-def setup_install():
-  print('Installing pip and virtualenv so we can give django its own version of python')
-  os.system('yum -y install python-pip && pip install --upgrade pip')
-  os.system('pip install virtualenv')
-  os.chdir('/opt')
-  os.mkdir('/opt/django')
-  os.chdir('/opt/django')
-  os.system('virtualenv django-env')
-  os.system('adduser -M django && usermod -L django')
 
 def local_repo():
     repo="""[local-epel]
-Name=NTI300 EPEL
-baseurl=http://35.192.184.169/epel/
+name=NTI300 EPEL
+baseurl=http://35.222.109.172/epel/
 gpgcheck=0
 enabled=1"""
     os.system('for file in $( ls /etc/yum.repos.d/ ); do mv /etc/yum.repos.d/$file /etc/yum.repos.d/$file.bak; done')
     print(repo)
-    with open('/etc/yum.repos.d/local-repo.repo',"w+") as f:
+    with open("/etc/yum.repos.d/local-repo.repo","w+") as f:
       f.write(repo)
     f.close()
 
+
+def setup_install():
+    print('installing pip and virtualenv so we can give django its own version of python')
+    os.system('yum -y install python-pip && pip install --upgrade pip')
+    os.system('pip install virtualenv')
+    os.chdir('/opt')
+    os.mkdir('/opt/django')
+    os.chdir('/opt/django')
+    os.system('virtualenv django-env')
+
+
 def django_install():
-  print('Activating virtualenv and installing django after pre-reqs have been met')
-  os.system('source /opt/django/django-env/bin/activate && pip install django')
-  os.chdir('/opt/django')
-  os.system('source /opt/django/django-env/bin/activate ' + \
-    '&& django-admin --version ' + \
-    '&& django-admin startproject project1' + \
-    '&& chown -R django /opt/django')
+    print('activating virtualenv and installing django after pre-requirements have been met')
+    # You must activate the virtualenv shell every time you perform a command in order for it to work from python
+    os.system('source /opt/django/django-env/bin/activate && pip install django')
+    # Confirm install and start a django project
+    os.chdir('/opt/django')
+    os.system('source /opt/django/django-env/bin/activate ' + \
+              '&& django-admin --version ' + \
+              '&& django-admin startproject project1')
+    os.system('adduser -M django')
+    os.system('usermod -L django')
+    os.system('chown -R django /opt/django')
+
 
 def django_start():
-  print('Starting django service')
-  os.chdir('/opt/django/project1')
-  os.system('source /opt/django/django-env/bin/activate ' + \
-    '&& python manage.py migrate')
-  os.system('source /opt/django/django-env/bin/activate && echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\',\'admin@project1.com\',\'NTI300NTI300\') | python manage.py shell')
-  outputwithnewline = subprocess.check_output('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Address: //\' -e \'s/<.*$//\'', shell=True)
-  print (outputwithnewline)
-  output = outputwithnewline.replace("\n","")
-  old_string = 'ALLOWED_HOSTS = []'
-  new_string = 'ALLOWED_HOSTS = [\'{}\']'.format(output)
-  print (old_string)
-  print (new_string)
-  with open('project1/settings.py') as f: newText=f.read().replace(old_string, new_string)
-  with open('project1/settings.py', "w") as f: f.write(newText)
-  f.close()
-  os.system('sudo -u django sh -c "source /opt/django/django-env/bin/activate && python manage.py runserver 0.0.0.0:8000&"')
+    print('starting django')
+    os.system('chown -R django /opt/django')
+    os.chdir('/opt/django/project1')
+    os.system('source /opt/django/django-env/bin/activate ' + \
+              '&& python manage.py migrate')
+    os.system('source /opt/django/django-env/bin/activate && echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\'admin\', \'admin@newproject.com\',\'NTI300NTI300\')" | python manage.py shell')
+
+    outputwithnewline = subprocess.check_output('curl -s checkip.dyndns.org | sed -e \'s/.*Current IP Address: //\' -e \'s/<.*$//\' ', shell=True)
+    print (outputwithnewline)
+    output = outputwithnewline.replace("\n", "")
+    old_string = "ALLOWED_HOSTS = []"
+    new_string = 'ALLOWED_HOSTS = [\'{}\']'.format(output)
+    print (old_string)
+    print (new_string)
+
+    with open('project1/settings.py') as f:
+        newText=f.read().replace(old_string, new_string)
+    with open('project1/settings.py', "w") as f:
+        f.write(newText)
+        f.close()
+
+    os.system('sudo -u django sh -c "source /opt/django/django-env/bin/activate && python manage.py runserver 0.0.0.0:8000&"')
+
 local_repo()
 setup_install()
 django_install()
